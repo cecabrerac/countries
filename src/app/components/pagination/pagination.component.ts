86524a974620ad3b7597052e09032cc1c1fe9c64
cecabrerac <ccabrera9@hotmail.com>
2023-09-28 11:34:23 -0500

    pagination comp

---
+++ src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
