dace24714e9f7f53570851a84fafbe5db44b4f5d
cecabrerac <ccabrera9@hotmail.com>
2023-09-29 10:01:33 -0500

    main and card components updates

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  apiUrl = environment.url;
+  public dataCards: any;
+  public firstTen: any;
+  constructor(private restService: RestService) {}
 
   ngOnInit(): void {
+    this.getAllCountries();
   }
 
+  getAllCountries() {
+    this.restService.get(this.apiUrl).subscribe((respuesta: any) => {
+      this.dataCards = respuesta;
+      this.firstTen = this.dataCards.slice(0, 30);
+      console.log(this.dataCards);
+      console.log(this.firstTen);
+    });
+  }
 }
