0c79e7974601f0ecbc80d24e089f7284358c459d
cecabrerac <ccabrera9@hotmail.com>
2023-10-01 20:13:16 -0500

    import NgxPaginationModule

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NbCardModule, NbButtonModule } from '@nebular/theme';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { PaginationComponent } from 'src/app/components/pagination/pagination.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 const routes: Routes = [{ path: '', component: MainComponent }];
 
@@ -14,6 +15,7 @@ const routes: Routes = [{ path: '', component: MainComponent }];
     CommonModule,
     NbCardModule,
     NbButtonModule,
+    NgxPaginationModule,
     RouterModule.forChild(routes),
   ],
   exports: [RouterModule],
