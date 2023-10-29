24982dab52c5f6a7a6bd904d4fed324f135d2b55
cecabrerac <ccabrera9@hotmail.com>
2023-10-29 10:44:34 -0500

    migration to angular 16 ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'main'
+},
+  
   {
     path: 'main',
     loadChildren: () =>
