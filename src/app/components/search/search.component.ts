cd43cfd8eb8070afe252ae9d2c2631243c25a5ed
cecabrerac <ccabrera9@hotmail.com>
2023-10-31 19:11:30 -0500

    setting from control

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+
+  public searchText = new FormControl('');
 
 }
