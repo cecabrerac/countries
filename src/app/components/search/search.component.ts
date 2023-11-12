308f48464be941c84582859733186c3793927d8f
cecabrerac <ccabrera9@hotmail.com>
2023-11-12 14:30:41 -0500

    clean code

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SearchService } from 'src/app/search.service';
 
