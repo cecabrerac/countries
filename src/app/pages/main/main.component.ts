e9ef0e19eb0b60346e81d025f5a8a2e74e195cf6
cecabrerac <ccabrera9@hotmail.com>
2023-11-14 16:39:42 -0500

    filters ok

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit, DoCheck, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 import { RestService } from 'src/app/services/rest.service';
 import { environment } from '../../../environments/environment';
 import { SearchService } from 'src/app/search.service';
@@ -14,40 +14,43 @@ export class MainComponent implements OnInit, DoCheck {
   itemsPerPage: number = 21;
   apiUrl = environment.url;
   public dataCards: any = [];
+  public sortedData: any = [];
   public filteredData: any = [];
   public toShow: any = [];
   searchText: string = '';
-
-  //continents
   continent: string = '';
 
-  constructor(private restService: RestService, private searchService: SearchService, private continentService: ContinentService) {}
+  constructor(private restService: RestService, private searchService: SearchService, private continentService: ContinentService, private cdRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+   
     if (this.dataCards.length == 0) this.getAllCountries();
-    this.searchText = this.searchService.getSearchText()
-    this.continent = this.continentService.getSelectedValue()
-    console.log(this.continent)
-    this.filteredData = this.getAllCountries();
   }
 
   ngDoCheck(){
     this.searchText = this.searchService.getSearchText()
-    if (this.filteredData) this.toShow = this.filteredData.filter((item: any) => item.name.common.toLowerCase().includes(this.searchText.toLowerCase()));
     this.continent = this.continentService.getSelectedValue()
+    console.log(this.searchText)
     console.log(this.continent)
+    console.log(this.sortedData)
+       
+    if (this.sortedData) this.filteredData = this.sortedData.filter((item: any) => item.name.common.toLowerCase().includes(this.searchText.toLowerCase()));
+    console.log(this.toShow);
     if (this.filteredData) this.toShow = this.filteredData.filter((item: any) => item.continents[0].toLowerCase().includes(this.continent.toLowerCase()));
+    if(this.continent == 'All') this.toShow = this.filteredData;
+    
+    this.cdRef.detectChanges();
   }
 
   getAllCountries() {
     this.restService.get(this.apiUrl).subscribe((respuesta: any) => {
       this.dataCards = respuesta;
-      const sortedData = this.dataCards.sort((a: any, b: any) => {
+      this.sortedData = this.dataCards.sort((a: any, b: any) => {
         const aName = a.name.common;
         const bName = b.name.common;
         return aName < bName ? -1 : aName > bName ? 1 : 0;
       });
-      this.filteredData = sortedData.filter((item: any) => item.name.common.includes(this.searchText));
+      // this.filteredData = this.sortedData.filter((item: any) => item.name.common.includes(this.searchText));
       // this.toShow = sortedData.slice(0, 200);
     });
   }
